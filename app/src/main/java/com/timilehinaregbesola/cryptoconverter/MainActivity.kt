8908d663d127a8a3f1ff67611dc25bea2ffac716
Timilehin <aregbestimi@gmail.com>
2022-02-26 22:51:07 +0100

    updated cards

--- app/src/main/java/com/timilehinaregbesola/cryptoconverter/MainActivity.kt
+++ app/src/main/java/com/timilehinaregbesola/cryptoconverter/MainActivity.kt
@@ -3,7 +3,19 @@ package com.timilehinaregbesola.cryptoconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
@@ -13,6 +25,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.timilehinaregbesola.cryptoconverter.ui.theme.CryptoConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,15 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(
-                                title = { Text("Converter", color = Color.White) },
-                                backgroundColor = Color.Black
-                            )
-                        }
-                    ) {
-                    }
+                    HomeScreen()
                 }
             }
         }
@@ -41,14 +47,106 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Converter", color = Color.White) },
+                backgroundColor = Color.Black
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFF050506))
+                .padding(horizontal = 32.dp, vertical = 24.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                FeaturedCoinCard(
+                    backgroundColor = Color(0xFFA5CAE3),
+                    name = "Bitcoin" to "BTC",
+                    percentage = 6,
+                    amount = 6.0286
+                )
+                FeaturedCoinCard(
+                    backgroundColor = Color(0xFFCEB7F0),
+                    name = "Dogecoin" to "DOGE",
+                    percentage = 118,
+                    amount = 16.800
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                FeaturedCoinCard(
+                    backgroundColor = Color(0xFFE4E4E5),
+                    name = "Ethereum" to "ETH",
+                    percentage = 18,
+                    amount = 0.0086
+                )
+                FeaturedCoinCard(
+                    backgroundColor = Color(0xFF383F48),
+                    name = "Dollars" to "USDT",
+                    percentage = 78,
+                    amount = 7.860,
+                    textColor = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun FeaturedCoinCard(
+    backgroundColor: Color,
+    name: Pair<String, String>,
+    percentage: Int,
+    amount: Double,
+    textColor: Color = Color.Black
+) {
+    Card(
+        modifier = Modifier
+            .width(150.dp)
+            .height(150.dp),
+        backgroundColor = backgroundColor,
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .weight(1f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = name.second, color = textColor)
+                Text(text = "+$percentage%", color = Color(0xFFA2A2A6))
+            }
+            Text(modifier = Modifier.weight(1f), text = amount.toString(), fontSize = 16.sp)
+            Column(modifier = Modifier.weight(3f), verticalArrangement = Arrangement.Bottom) {
+                Text(
+                    text = name.first,
+                    color = textColor
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CryptoConverterTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
